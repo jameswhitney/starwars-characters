5531b7204778133555d5a49d71241b823e7770e6
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2020-01-20 14:04:39 -0500

    Fixes issue with text on Cards. Issue: correct image layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,14 @@ const Card = ({ id, name, email }) => {
   return(
     // Style for card uses tachyons instead of creating css file.
     //
-    <article className="dib br3 pa2 ma4 b--black-30 grow-large shadow-4">
-      <div className="flex items-end">
-        <img className="br-100 h4 w4 ba bw2 b--black-40 pa2 bg-washed-blue" 
+    <article className="dib br3 pa2 ma2 b--black-30 grow-large shadow-4">
+      <div className="flex items-right">
+        <img className="tc br-100 h3 w3 ba bw2 b--black-40 pa2 bg-washed-blue" 
           alt='robots'
           // dicebear.com used to generated random avatars for cards
           src={`https://avatars.dicebear.com/v2/bottts/${id}.svg`} 
         />
-        <div className="tc pa2 ma2 white">
+        <div className="f5 pa2 tc white">
           <h3>{name}</h3>
           <h4>{email}</h4> 
         </div>
