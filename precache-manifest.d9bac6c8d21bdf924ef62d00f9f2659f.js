19328b64cfb86db3d4b35673fd9e5c7321d41350
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2020-01-23 16:38:41 -0500

    Updates

---
+++ precache-manifest.d9bac6c8d21bdf924ef62d00f9f2659f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58b3657f771fb40219b888290c340ff",
+    "url": "/starwars-characters/index.html"
+  },
+  {
+    "revision": "cf74a064930cdf8d4656",
+    "url": "/starwars-characters/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "55588dec903e85169442",
+    "url": "/starwars-characters/static/css/main.820b7b91.chunk.css"
+  },
+  {
+    "revision": "cf74a064930cdf8d4656",
+    "url": "/starwars-characters/static/js/2.c09b453a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starwars-characters/static/js/2.c09b453a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55588dec903e85169442",
+    "url": "/starwars-characters/static/js/main.8adc4569.chunk.js"
+  },
+  {
+    "revision": "081af0eab48f6bd89585",
+    "url": "/starwars-characters/static/js/runtime-main.2f05b040.js"
+  },
+  {
+    "revision": "a34484e451f48ba907f138318c3a5073",
+    "url": "/starwars-characters/static/media/jaapokkisubtract-regular.a34484e4.woff"
+  }
+]);
